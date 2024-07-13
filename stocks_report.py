fb62fcd148f6993ccb58b845a3a38562a293e21e
harvey <harveyreginaldspectre1@gmail.com>
2024-07-13 11:56:51 +0530

    refactor async code

--- stocks_report.py
+++ stocks_report.py
@@ -6,12 +6,14 @@ import time
 
 financialModelingPrepKey = 'xyz'
 
+
 start = time.time()
 
 async def data_request(ticker):
     async with aiohttp.ClientSession() as session:
-        resp = await session.get(f'https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?from=1990-10-10&apikey={financialModelingPrepKey}')
-        dic = await resp.json()        
+        resp = await session.get(f'https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?apikey={financialModelingPrepKey}')
+        # print(ticker, resp.status_code)
+        dic = await resp.json()
     return dic
 
 async def data_collection():
@@ -19,25 +21,11 @@ async def data_collection():
     stocks_tickers = ['AAPL', 'MSFT', 'TSLA', 'NVDA', 'GOOG']    
     stocks_data = {}
 
-    AAPL= asyncio.create_task(data_request('AAPL'))
-    MSFT= asyncio.create_task(data_request('MSFT'))
-    TSLA= asyncio.create_task(data_request('TSLA'))
-    NVDA= asyncio.create_task(data_request('NVDA'))
-    GOOG= asyncio.create_task(data_request('GOOG'))
+    results = await asyncio.gather(data_request('AAPL'), data_request('MSFT'), data_request('TSLA'), data_request('NVDA'), data_request('GOOG'))
 
-    AAPL_data = await AAPL
-    MSFT_data = await MSFT
-    TSLA_data = await TSLA
-    NVDA_data = await NVDA
-    GOOG_data = await GOOG    
-
-    stocks_data['AAPL'] = AAPL_data['historical']
-    stocks_data['MSFT'] = MSFT_data['historical']
-    stocks_data['TSLA'] = TSLA_data['historical']
-    stocks_data['NVDA'] = NVDA_data['historical']
-    stocks_data['GOOG'] = GOOG_data['historical']
-
-    return stocks_data
+    for i in results:
+        print (i['historical'][0])
+    return 0 #stocks_data
 
 asyncio.run(data_collection())
 
