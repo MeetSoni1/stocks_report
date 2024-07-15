837b65c8ad5b241615e1a9ee4bdb1942eff33600
harvey <harveyreginaldspectre1@gmail.com>
2024-07-15 19:08:21 +0530

    modify

--- stocks_report.py
+++ stocks_report.py
@@ -3,32 +3,55 @@ import json
 import asyncio
 import aiohttp
 import time
+from time import perf_counter
 
-financialModelingPrepKey = 'xyz'
+alphaVantageKey = 'xyz'
 
+tickers = ['KPITTECH.BSE', 'PERSISTENT.BSE', 'NEWGEN.BSE', 'TEJASNET.BSE', 'ADANIGREEN.BSE', 'ADANIPOWER.BSE', 'TATAPOWER.BSE', 'JPPOWER.BSE', 'VEDL.BSE', 'RELIANCE.BSE']
 
-start = time.time()
+async def dataRequest(ticker : str) -> dict:
+    print('Request sent')
+    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol={ticker}&apikey={alphaVantageKey}'
 
-async def data_request(ticker):
     async with aiohttp.ClientSession() as session:
-        resp = await session.get(f'https://financialmodelingprep.com/api/v3/historical-price-full/{ticker}?apikey={financialModelingPrepKey}')
-        # print(ticker, resp.status_code)
-        dic = await resp.json()
-    return dic
+        response = await session.get(url)
+        data = await response.json()
+    
+    return data
 
-async def data_collection():
-    key = financialModelingPrepKey
-    stocks_tickers = ['AAPL', 'MSFT', 'TSLA', 'NVDA', 'GOOG']    
-    stocks_data = {}
+async def dataCollection() -> list:
+    
+    results = await asyncio.gather(dataRequest('KPITTECH.BSE'), dataRequest('PERSISTENT.BSE'), dataRequest('NEWGEN.BSE'), dataRequest('TEJASNET.BSE'), dataRequest('ADANIGREEN.BSE'), dataRequest('ADANIPOWER.BSE'), dataRequest('TATAPOWER.BSE'), dataRequest('JPPOWER.BSE'), dataRequest('VEDL.BSE'), dataRequest('RELIANCE.BSE'))
 
-    results = await asyncio.gather(data_request('AAPL'), data_request('MSFT'), data_request('TSLA'), data_request('NVDA'), data_request('GOOG'))
+    print('Results recieved')
+    return results
 
-    for i in results:
-        print (i['historical'][0])
-    return 0 #stocks_data
+stocksData = asyncio.run(dataCollection())
 
-asyncio.run(data_collection())
+def weeklyPriceReport(price_weekly: dict) -> dict:
+    lastWeekKey = list(price_weekly['Weekly Time Series'].keys())[0]
+    lastWeek = price_weekly['Weekly Time Series'][lastWeekKey]
 
-end = time.time()
-total_time = end-start
-print(total_time)
+    for i in lastWeek.keys():
+        if 'open' in i.lower():
+            open = lastWeek[i]
+        if 'close' in i.lower():
+            close = lastWeek[i]
+
+    percentChangePrice = round(((float(close)-float(open))/float(open))*100, 2)
+
+    lastWeek['percentChangePrice'] = percentChangePrice
+    return lastWeek
+
+lst = []
+
+print('Preparing report')
+
+for i in stocksData:
+    lst.append(weeklyPriceReport(i))
+
+print('Report ready')
+
+stockReport = list(zip(tickers, lst))
+
+print(stockReport)
