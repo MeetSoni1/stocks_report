69ad2e33e9520e7eae5d02e9b34eeafc7c226ab7
harvey <harveyreginaldspectre1@gmail.com>
2024-07-28 20:13:56 +0530

    add terraform code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,126 @@
-# Eventbridge
-# Lambda
-# S3
-# SNS
-# Cloudwatch
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    #   version = "~> 5.0"
+    }
+  }
+}
 
-# Policies
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+# EVENTBRIDGE
+    # IAM Role
+        # Policies
+            # Event bridge scheduler (default)
+
+resource "aws_scheduler_schedule" "example" {
+  name       = "my-schedule"
+  group_name = "default"
+
+  flexible_time_window {
+    mode = "OFF"
+  }
+
+  schedule_expression = "rate(1 hours)"
+
+  target {
+    arn      = aws_sqs_queue.example.arn
+    role_arn = aws_iam_role.example.arn
+  }
+}
+
+
+# LAMBDA
+    # IAM Role
+        # Policies
+            # Lambda Basic Excecution(default)
+            # Parameter Store
+            # S3 put object
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "lambda.js"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "nodejs18.x"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
+
+
+# PARAMETER STORE
+resource "aws_ssm_parameter" "secret" {
+  name        = "/production/database/password/master"
+  description = "The parameter description"
+  type        = "SecureString"
+  value       = var.database_master_password
+
+#   tags = {
+#     environment = "production"
+#   }
+}
+
+
+# S3
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+}
+
+
+# SNS
+    # Access Policy
+
+resource "aws_sns_topic" "user_updates" {
+  name = "user-updates-topic"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = "arn:aws:sns:us-west-2:432981146916:user-updates-topic"
+  protocol  = "sqs"
+  endpoint  = "arn:aws:sqs:us-west-2:432981146916:terraform-queue-too"
+}
+
+# Cloudwatch
